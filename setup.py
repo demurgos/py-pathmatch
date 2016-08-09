1fbef81bfd614a44f2b529141676393e36341b9e
Charles Samborski <samborski.charles@gemalto.com>
2016-08-09 13:38:42 +0200

    Release v0.1.1

--- setup.py
+++ setup.py
@@ -22,13 +22,13 @@ with codecs.open(os.path.join(PROJECT_ROOT, u'README.rst'), encoding=u'UTF-8') a
 
 setup(
     name=u'pathmatch',
-    version=u'0.1.0',
+    version=u'0.1.1',
     description=u'Path matching utilities',
     long_description=LONG_DESCRIPTION,
     author=u'Charles Samborski',
     author_email=u'demurgos.net@gmail.com',
     url = 'https://github.com/demurgos/py-pathmatch',
-    download_url = 'https://github.com/demurgos/py-pathmatch/tarball/v0.1.0',
+    download_url = 'https://github.com/demurgos/py-pathmatch/tarball/v0.1.1',
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         u'Development Status :: 3 - Alpha',
